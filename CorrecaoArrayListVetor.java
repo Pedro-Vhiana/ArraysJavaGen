41e94f079c8f66d9e801d1deaad56c1b55313dab
Pedro-Vhiana <pedrohb.0222@gmail.com>
2023-11-17 01:47:54 -0300

    Adicionando as correções de algumas atividades - Generation

---
+++ CorrecaoArrayListVetor.java
@@ -0,0 +1,34 @@
+package ArraysJava;
+
+import java.util.Scanner;
+
+public class CorrecaoArrayListVetor {
+	
+	// ArrayList
+
+	public static void main(String[] args) {
+		
+		int vet[] = new int[10];
+		int somaVetor=0,x;
+		float mediaVet;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		for(x=0;x<10;x++) {
+			System.out.println("\nEntre com um número: ");
+			vet[x] = leia.nextInt();
+			if(x % 2 == 1) {
+				System.out.println("\nElemento de índice impar: "+vet[x]);
+			}
+			if(vet[x] % 2 == 0) {
+				System.out.println("\nO número: "+vet[x]+" é um elemento par...");
+			}
+			somaVetor += vet[x];
+		}
+		mediaVet = somaVetor / x;
+		System.out.println("\nSoma dos elementos do vetor: "+somaVetor);
+		System.out.println("\nA média dos elementos do vetor é: "+mediaVet);
+
+	}
+
+}
